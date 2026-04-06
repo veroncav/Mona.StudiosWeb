4e37abbba0457e7f098504f0266c2d50d591ee95
veroncav <veronika1999.v@gmail.com>
2026-04-06 21:37:10 +0300

    fix: reset scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
@@ -22,6 +23,7 @@ function App() {
     return (
         <>
             <Header />
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
